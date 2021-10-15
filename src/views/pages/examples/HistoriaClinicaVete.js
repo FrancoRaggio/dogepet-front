f00de024f9f9da91a2ff4d6340e441ad0e01588e
martin-tixcode <martin@tixcode.co>
2021-10-15 18:37:33 -0300

    historia clinica

--- src/views/pages/examples/HistoriaClinicaVete.js
+++ src/views/pages/examples/HistoriaClinicaVete.js
@@ -1,154 +1,64 @@
-/*!
-
-=========================================================
-* Argon Dashboard PRO React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-pro-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 // reactstrap components
 import {
-  Button,
-  Card,
   CardHeader,
-  CardBody,
-  CardImg,
-  CardTitle,
-  FormGroup,
-  Collapse,
-  ButtonGroup,
   UncontrolledTooltip,
   Table,
-  Form,
-  Input,
-  ListGroupItem,
-  pagination,
-  componentRef,
-  copyToClipboardAsTable,
-  ListGroup,
-  Progress,
   Container,
   Row,
   Col,
 } from "reactstrap";
-// core components
-import ProfileHeader from "components/Headers/ProfileHeader.js";
-import ReactToPrint from "react-to-print";
-// react component for creating dynamic tables
-import BootstrapTable from "react-bootstrap-table-next";
-import paginationFactory from "react-bootstrap-table2-paginator";
-import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
-// react component used to create sweet alerts
-import ReactBSAlert from "react-bootstrap-sweetalert";
-
-import SimpleHeader from "components/Headers/SimpleHeader.js";
-
-import { dataTable } from "variables/general";
+import { Search } from "react-bootstrap-table2-toolkit";
+import { RepositoryFactory } from "repositories/RepositoryFactory";
+const userRepository = RepositoryFactory.get('user');
 
 const { SearchBar } = Search;
 
-
 function HistoriaClinica() {
-  const [alert, setAlert] = React.useState(null);
 
-  const componentRef = React.useRef(null);
-  const copyToClipboardAsTable = (el) => {
-    var body = document.body,
-      range,
-      sel;
-    if (document.createRange && window.getSelection) {
-      range = document.createRange();
-      sel = window.getSelection();
-      sel.removeAllRanges();
-      try {
-        range.selectNodeContents(el);
-        sel.addRange(range);
-      } catch (e) {
-        range.selectNode(el);
-        sel.addRange(range);
-      }
-      document.execCommand("copy");
-    } else if (body.createTextRange) {
-      range = body.createTextRange();
-      range.moveToElementText(el);
-      range.select();
-      range.execCommand("Copy");
-    }
-    setAlert(
-      <ReactBSAlert
-        success
-        style={{ display: "block", marginTop: "-100px" }}
-        title="Good job!"
-        onConfirm={() => setAlert(null)}
-        onCancel={() => setAlert(null)}
-        confirmBtnBsStyle="info"
-        btnSize=""
-      >
-        Copied to clipboard!
-      </ReactBSAlert>
-    );
-  };
+  const [alert, setAlert] = React.useState(null);
+  const [users, setUsers] = useState([])
+
+  const getUsers = async () => {
+    let data =  await userRepository.getUsers()
+    setUsers(data)
+    return
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
     <>
-     <Container className="mt--0" >
-       <row>
-       <Col className="order-xl-1" xl="13">
-              <CardHeader>
-                <Row className="align-items-center">
-                  <Col xs="8">
-                    <h3 className="mb-0">Historial Clinico</h3>
-                  </Col>
-                </Row>
-              </CardHeader>
+      <Container className="mt--0" >
+        <row>
+          <Col className="order-xl-1" xl="13">
+            <CardHeader>
+              <Row className="align-items-center">
+                <Col xs="8">
+                  <h3 className="mb-0">Historial Clinico</h3>
+                </Col>
+              </Row>
+            </CardHeader>
 
-              <Col xs={12} sm={6}>
-                          <div
-                            id="datatable-basic_filter"
-                            className="dataTables_filter px-9 pb-1 float-left "
-                          >
-                            <label>
-                              Search:
-                              <SearchBar
-                                className="form-control-sm"
-                                placeholder=""
-                                // {...props.searchProps}
-                              />
-                            </label>
-                          </div>
-                        </Col>
-              </Col>
-              <div className="pl-lg-3 bg-white  "> 
-     <Table class="table table-responsive table-striped text-center">
-            <thead className="thead-light">
-              <tr>
-                <th>Cliente</th>
-                <th>DNI</th>
-                {/* <th>Product</th> */}
-                <th />
-              </tr>
-            </thead>
-            <tbody className="">
-              <tr>
-                <td className="table-user">
-                  <img
-                    alt="..."
-                    className="avatar rounded-circle mr-3"
-                    src={require("assets/img/theme/team-m.png").default}
+            {/* <Col xs={12} sm={6}>
+              <div
+                id="datatable-basic_filter"
+                className="dataTables_filter px-9 pb-1 float-left "
+              >
+                <label>
+                  Search:
+                  <SearchBar
+                    className="form-control-sm"
+                  // placeholder=""
+                  // {...props.searchProps}
                   />
                 </label>
               </div>
-           
+            </Col> */}
+          </Col>
           <div className="pl-lg-3 bg-white  ">
             <Table class="table table-responsive table-striped text-center">
               <thead className="thead-light">
@@ -190,8 +100,8 @@ function HistoriaClinica() {
               </tbody>
             </Table>
           </div>
-          </row>
-          </Container>
+        </row>
+      </Container>
     </>
   );
 }
