066d6e2154f4d0084f20e1e531ee7f74ba300fcb
martin-tixcode <martin@tixcode.co>
2021-11-10 22:49:22 -0300

    add mascota

--- src/views/pages/examples/AddMascota.js
+++ src/views/pages/examples/AddMascota.js
@@ -77,12 +77,12 @@ const AddMascota = () => {
       photo: picture,
     }
     const form = new FormData()
-    form.append('name', firstName)
-    form.append('user_id', username)
-    form.append('race_id', race)
-    form.append('date', date)
-    form.append('color', color)
-    form.append('gender', gender)
+    // form.append('name', firstName)
+    // form.append('user_id', username)
+    // form.append('race_id', race)
+    // form.append('date', date)
+    // form.append('color', color)
+    // form.append('gender', gender)
     form.append('photo', picture)
 
     await petRepository.createPet(form);
